eed4e39abc793e277180b613f50a1347dfe5fd61
Mahmoud20-00491 <noreldienm494@gmail.com>
2024-01-27 16:22:17 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_application_3/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.flutter_application_3
+
+import android.view.WindowManager.LayoutParams
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity: FlutterActivity() {
+   
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
+  runApp(MyAppImpl());
+}
+
+abstract class MyAppBase extends StatelessWidget {
+  void registerWebViewWebImplementation();
+
+  const MyAppBase({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    registerWebViewWebImplementation();
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Homepage(),
+    );
+  }
+}
+
+class MyAppImpl extends MyAppBase {
+  @override
+  void registerWebViewWebImplementation() {
+    WebView.platform = SurfaceAndroidWebView();
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final _key = UniqueKey();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebView(
+          key: _key,
+          initialUrl: 'https://nabdelqanon.com/home/',
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
+  }
+}
